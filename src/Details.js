f14fe2cb315f676e8747c8527c086490ab19e60f
Govind Vishwakarma <vishwakarmagovind22@gmail.com>
2021-06-27 21:21:44 +0530

    Added Error Boundary to catch errors.

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
@@ -50,4 +51,12 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  );
+}
